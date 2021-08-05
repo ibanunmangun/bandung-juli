bc61f6be10b4d680c79791c17eba97130629928c
ibanunmangun <62372980+ibanunmangun@users.noreply.github.com>
2021-08-05 18:14:21 +0800

    Add files via upload

---
+++ data/pointofinterestbandung_2.js
@@ -0,0 +1,18 @@
+var json_pointofinterestbandung_2 = {
+"type": "FeatureCollection",
+"name": "pointofinterestbandung_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "name": "Bubur Ayam Pak Otong", "desc": "Bubur Ayam Pak Otong, Jl. Jend. Sudirman No.451, Ciroyom, Bojongloa Kaler, Bandung City, West Java 40231, Indonesia", "X": null, "Y": null, "Info": "point of interest" }, "geometry": { "type": "Point", "coordinates": [ 107.5888936, -6.9191175 ] } },
+{ "type": "Feature", "properties": { "name": "Pasar Antik Cikapundung", "desc": "Pasar Antik Cikapundung, Ps. Cikapundung Elektrik Center, Blok, Jl. ABC Blok U-1, Braga, Sumurbandung, Bandung City, West Java 40111, Indonesia", "X": null, "Y": null, "Info": "point of interest" }, "geometry": { "type": "Point", "coordinates": [ 107.6087129, -6.9189022 ] } },
+{ "type": "Feature", "properties": { "name": "Warung Nasi C’Mar", "desc": "Warung Nasi C’Mar, Coblong, Jl. ABC No.21, Dago, Kec. Sumur Bandung, Kota Bandung, Jawa Barat 40111, Indonesia", "X": null, "Y": null, "Info": "point of interest" }, "geometry": { "type": "Point", "coordinates": [ 107.6088857, -6.9195735 ] } },
+{ "type": "Feature", "properties": { "name": "Jalan Asia Afrika", "desc": "Jalan Asia Afrika, Jl. Asia Afrika No.69, Braga, Kec. Lengkong, Kota Bandung, Jawa Barat 40261, Indonesia", "X": null, "Y": null, "Info": "point of interest" }, "geometry": { "type": "Point", "coordinates": [ 107.6099017, -6.9213843 ] } },
+{ "type": "Feature", "properties": { "name": "MCDUREN", "desc": "MCDUREN, Jl. Cikapundung Barat No.1, Braga, Sumurbandung, Bandung City, West Java 40111, Indonesia", "X": null, "Y": null, "Info": "point of interest" }, "geometry": { "type": "Point", "coordinates": [ 107.6079842, -6.9205299 ] } },
+{ "type": "Feature", "properties": { "name": "Perkedel Bondon", "desc": "Perkedel Bondon, Jl. Stasiun Timur No.14, Kebon Jeruk, Andir, Bandung City, West Java 40181, Indonesia", "X": null, "Y": null, "Info": "point of interest" }, "geometry": { "type": "Point", "coordinates": [ 107.6026022, -6.9155762 ] } },
+{ "type": "Feature", "properties": { "name": "Rumah Sakit Kebonjati", "desc": null, "X": null, "Y": null, "Info": "public facilities" }, "geometry": { "type": "Point", "coordinates": [ 107.59631, -6.9161 ] } },
+{ "type": "Feature", "properties": { "name": "Polsek Andir", "desc": null, "X": null, "Y": null, "Info": "public facilities" }, "geometry": { "type": "Point", "coordinates": [ 107.59775, -6.91695 ] } },
+{ "type": "Feature", "properties": { "name": "SPBU Tamblong", "desc": null, "X": null, "Y": null, "Info": "public facilities" }, "geometry": { "type": "Point", "coordinates": [ 107.61199, -6.9199 ] } },
+{ "type": "Feature", "properties": { "name": "Masjid Agung Bandung", "desc": null, "X": null, "Y": null, "Info": "public facilities" }, "geometry": { "type": "Point", "coordinates": [ 107.60648, -6.92132 ] } },
+{ "type": "Feature", "properties": { "name": "Area Parkir Alun-alun Bandung Bagian Timur", "desc": null, "X": null, "Y": null, "Info": "public facilities" }, "geometry": { "type": "Point", "coordinates": [ 107.608332, -6.921993 ] } }
+]
+}
